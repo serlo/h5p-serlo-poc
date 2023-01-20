d6204f3d2b6d88cc0a9880788116cfd6ecd9292c
Entkenntnis <Entkenntnis@users.noreply.github.com>
2023-01-20 13:10:01 +0100

    make more robust

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { renderArticle } from "./article-renderer";
 
 export default function App(props) {
+  let obj = [];
+
+  try {
+    obj = JSON.parse(atob(props.json));
+  } catch (e) {}
+
   return (
     <div>
       <h1>Hello, World!</h1>
-      <div>{renderArticle(JSON.parse(atob(props.json)))}</div>
+      <div>{renderArticle(obj)}</div>
     </div>
   );
 }
